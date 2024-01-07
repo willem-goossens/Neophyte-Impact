d63d9d3eaaab12fb56686b231ac9e265cdd194fb
Willem Goossens <willem1305@gmail.com>
2024-01-07 11:07:56 +0100

    Foreach working

--- 3.2_GLONAF_region.R
+++ 3.2_GLONAF_region.R
@@ -15,13 +15,14 @@ library(lwgeom)
 library(terra)
 library(diffdf)
 library(doParallel)
+library(arsenal)
 
 ##### DATA ####
 ###### header ######
 header <- read_delim("../EVA Data/171_NeophyteInvasions20230216_notJUICE_header.csv", "\t")
 fast <- T
 if(fast) {
-  header <- header[runif(length(header$PlotObservationID)) > 0.9999,]
+  header <- header[runif(length(header$PlotObservationID)) > 0.99,]
 }
 
 header <- header[!(is.na(header$Latitude) | is.na(header$Longitude)),]
@@ -48,15 +49,6 @@ plot(st_geometry(plotLocations), add=T)
 ###### joined ######
 sf_use_s2(T)
 joinedData<-st_join( plotLocations, newGlonaf[c(1:23,25:85),],join=st_within)
-which(is.na(joinedData$OBJIDsic))
-sf_use_s2(F)
-res <- st_join(plotLocations, newGlonaf[24,])
-any(!is.na(res$OBJIDsic))
-index<-which(!is.na(res$OBJIDsic))
-# We cannot merge Northern Ireland
-joinedData[index,] <- res[index,]
-
-
 # some data is duplicated because it is located on the edge of two countries and glonaf shapefile is not very accurate
 setdiff(joinedData$PlotObservationID, plotLocations$PlotObservationID)
 any(is.na(joinedData$PlotObservationID))
@@ -65,10 +57,19 @@ joinedData$PlotObservationID[duplicated(joinedData$PlotObservationID)]
 # Here we just assign it but has to be improved
 joinedData<-joinedData[!duplicated(joinedData$PlotObservationID),]
 
-pi= st_intersection(glonafRegions[glonafRegions$OBJIDsic=="457",],glonafRegions[glonafRegions$OBJIDsic=="1558",])
-plot(st_geometry(glonafRegions[glonafRegions$OBJIDsic=="457",]), col='blue')
-plot(st_geometry(glonafRegions[glonafRegions$OBJIDsic=="1558",]), add=T, col="yellow")
-plot(pi$geometry, add=T, col="red")
+# Northern Ireland cannot be merged using s2
+sf_use_s2(F)
+res <- st_join(plotLocations, newGlonaf[24,])
+any(!is.na(res$OBJIDsic))
+index<-which(!is.na(res$OBJIDsic))
+# We cannot merge Northern Ireland
+joinedData[index,] <- res[index,]
+Northern_Ireland<- res[!is.na(res$OBJIDsic),]
+
+#pi= st_intersection(glonafRegions[glonafRegions$OBJIDsic=="457",],glonafRegions[glonafRegions$OBJIDsic=="1558",])
+#plot(st_geometry(glonafRegions[glonafRegions$OBJIDsic=="457",]), col='blue')
+#plot(st_geometry(glonafRegions[glonafRegions$OBJIDsic=="1558",]), add=T, col="yellow")
+#plot(pi$geometry, add=T, col="red")
 
 percentNotAssignedPlots <- sum(is.na(joinedData$code))/length(joinedData$code)*100
 percentNotAssignedPlots
@@ -93,36 +94,36 @@ for(i in 1:length(boundingBoxes$OBJIDsic)) {
 remainingPlots$code <- NA
 remainingPlots$Distance <- -1
 
-begin<-Sys.time()  
-for(i in 1:length(remainingPlots$PlotObservationID)) {
-  region <- NA
-  distance <- set_units(1000000,m)
+#begin<-Sys.time()  
+#for(i in 1:length(remainingPlots$PlotObservationID)) {
+#  region <- NA
+#  distance <- set_units(1000000,m)
   
-  for(j in 1:length(glonafRegions$OBJIDsic)) {
-    distBoundingBox <- st_distance(boundingBoxes[j,], remainingPlots[i,]) #Calculate the distance
-    # Only if the distance to the bounding box is smalle than distanceThreshold we need to compute the real distance to the region
-    if(distBoundingBox < distanceThreshold){
-      dist <- st_distance(glonafRegions[j,], remainingPlots[i,])
-      if(dist < distanceThreshold & dist < distance) {
-        region <- glonafRegions$OBJIDsic[j]
-        distance <- dist
-      }
-    }
-  }
+#  for(j in 1:length(glonafRegions$OBJIDsic)) {
+#    distBoundingBox <- st_distance(boundingBoxes[j,], remainingPlots[i,]) #Calculate the distance
+#   # Only if the distance to the bounding box is smalle than distanceThreshold we need to compute the real distance to the region
+#   if(distBoundingBox < distanceThreshold){
+#     dist <- st_distance(glonafRegions[j,], remainingPlots[i,])
+#      if(dist < distanceThreshold & dist < distance) {
+#       region <- glonafRegions$OBJIDsic[j]
+#       distance <- dist
+#     }
+#   }
+# }
   # Print the progress
-  message(i/length(remainingPlots$PlotObservationID)*100, "% ", remainingPlots$Country[i], " ", region)
-  remainingPlots$code[i] <- region
-  remainingPlots$Distance[i] <- distance
-}
-end<-Sys.time()
-round(end-begin, 2)
+#  message(i/length(remainingPlots$PlotObservationID)*100, "% ", remainingPlots$Country[i], " ", region)
+#  remainingPlots$code[i] <- region
+# remainingPlots$Distance[i] <- distance
+#}
+#end<-Sys.time()
+#round(end-begin, 2)
 
-remainingPlots2<- remainingPlots
+#remainingPlots2<- remainingPlots
 
-allPlotsWithRegion <- joinedData[,c("PlotObservationID", "code", "Country")]
+#allPlotsWithRegion <- joinedData[,c("PlotObservationID", "code", "Country")]
 #allPlotsWithRegion[is.na(allPlotsWithRegion$code),] <- remainingPlots[,c("PlotObservationID", "code", "Country")]
 
-x<-allPlotsWithRegion[is.na(allPlotsWithRegion$code),]
+#x<-allPlotsWithRegion[is.na(allPlotsWithRegion$code),]
 
 ###### parallel #####
 # We will run it parallel
@@ -137,17 +138,32 @@ doParallel::registerDoParallel(cl = my.cluster)
 foreach::getDoParRegistered()
 foreach::getDoParWorkers()
 
+distanceThreshold <- set_units(10000,m)
+
+remainingPlots <- plotLocations[is.na(joinedData$code),]
+
+# Compute bounding boxes for the regions, this allows to decide faster, if a plot is outside the distanceThreshold from a region
+boundingBoxes <- glonafRegions
+
+for(i in 1:length(boundingBoxes$OBJIDsic)) {
+  boundingBoxes$geometry[i] <- st_as_sfc(st_bbox(boundingBoxes$geometry[i]))
+}
+
+remainingPlots$code <- NA
+remainingPlots$Distance <- -1
 
 remainingPlots <- plotLocations[is.na(joinedData$code),]
 remainingPlots$code <- NA
 remainingPlots$Distance <- -1
 
+length(remainingPlots$PlotObservationID)
 begin<-Sys.time() 
 remainingPlots[,35:36]<-foreach(i = 1:length(remainingPlots$PlotObservationID), .combine='rbind', .packages=c("dplyr","mgcv", "sf","units")) %dopar% {
   region <- NA
   distance <- set_units(1000000,m)
   
-  for(j in 1:length(glonafRegions$OBJIDsic)) {
+  for(j in 1:length(boundingBoxes$OBJIDsic)) {
+    sf_use_s2(F)
     distBoundingBox <- st_distance(boundingBoxes[j,], remainingPlots[i,]) #Calculate the distance
     # Only if the distance to the bounding box is smalle than distanceThreshold we need to compute the real distance to the region
     if(distBoundingBox < distanceThreshold){
@@ -165,10 +181,30 @@ remainingPlots[,35:36]<-foreach(i = 1:length(remainingPlots$PlotObservationID), 
 end<-Sys.time()
 round(end-begin, 2)  
 
-print(setdiff(remainingPlots$code, remainingPlots2$code))
-
+#print(setdiff(remainingPlots$code, remainingPlots2$code))
+sf_use_s2(T)
+st_distance(boundingBoxes[1:85,], remainingPlots[30,])
 
 parallel::stopCluster(cl = my.cluster)
+
+# merge the plots which lie within a region with those which are in proximity and write the result to file
+allPlotsWithRegion <- joinedData[,c("PlotObservationID", "code", "Country")]
+remainingPlots$code<- as.character(remainingPlots$code)
+allPlotsWithRegion[is.na(allPlotsWithRegion$code),] <- remainingPlots[,c("PlotObservationID", "code", "Country")]
+
+
+compareCountryLabelWithRegion <- function(country, region) {
+  message("Number of plots in region: ", sum(allPlotsWithRegion$Region == region, na.rm = T))
+  message("Number of plots in country: ", sum(allPlotsWithRegion$Country == country, na.rm = T))
+}
+
+# Compare the number of plots in a region with the number of plots which are in the "same" country, does it make sense?
+compareCountryLabelWithRegion("Albania", "Albania")
+compareCountryLabelWithRegion("Austria", "Austria")
+compareCountryLabelWithRegion("Netherlands", "Netherlands")
+compareCountryLabelWithRegion("Italy", "Italy")
+compareCountryLabelWithRegion("Italy", "Sicily")
+compareCountryLabelWithRegion("Italy", "Sardinia")
 
 ##### PLOT ####
 
