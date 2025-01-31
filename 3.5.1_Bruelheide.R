b416be810f6434ed3982f65470afb03abd679ae9
Willem Goossens <willem1305@gmail.com>
2025-01-31 13:43:23 +0100

    file for habitat classification

---
+++ 3.5.1_Bruelheide.R
@@ -0,0 +1,51 @@
+
+
+setwd('C:/Users/u0166342/Documents/Boeren/Impact/eva_neophytes/EIVE Data/ESy-master')
+source('code/prep.R') #### Loading packages
+
+### define expert file:
+expertfile <- "EUNIS-ESy-2020-06-08.txt" # latest version of the EUNIS system, used in Chytrý et al. 2020, AVS
+
+### Start  ######################
+### Read and parse the expert file
+#################################################################### #
+### Step 1: Parse membership formulas into membership expressions ####
+### Step 2: Add right-hand sides of membership expressions        ####
+###         where there are no right-hand side conditions
+#################################################################### #
+source('code/step1and2_load-and-parse-the-expert-file.R')
+
+######################################################## #
+### Input 2                                           ####
+### Read vegetation data file into a long table with class data.table and with columns named
+### "RELEVE_NR", "TaxonName", "Cover_Perc"
+### If you want to use your own data, please make sure to have the three columns.
+
+
+# ################################################################### #
+# # Step 3: Create a numerical plot x membership condition matrix  ####
+# ################################################################### #
+# Create an Array to collect all membership conditions 
+# of the left- and right-hand side
+plot.cond <- array(0, c(length(unique(obs$RELEVE_NR)), length(conditions)), 
+                   dimnames = list(as.character(unique(obs$RELEVE_NR)), conditions))
+
+
+###################################### #
+### Step 4: Aggregate taxon levels  ####
+###################################### #
+# also changed file to return all non found species as na
+source('code/step4_aggregate-taxon-levels.R')
+
+class(obs) <- "data.table"
+
+# ############################################################## #
+### Step 5: Solve the membership conditions                   ####
+### and fill in the numerical plot x membership condition matrix
+# ############################################################## #
+
+source('code/step3and5_extract-and-solve-membership-conditions.R')
+
+
+setwd("C:/Users/u0166342/Documents/Boeren/Impact/eva_neophytes/Neophyte-Impact")
+stopCluster(cl)
