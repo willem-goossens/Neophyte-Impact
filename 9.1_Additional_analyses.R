f2546125a64518aa32a8d5c64935facd0d1a7e67
willem-goossens <willem1305@gmail.com>
2025-04-23 18:10:44 +0200

    Adapt new

--- 9.1_Additional_analyses.R
+++ 9.1_Additional_analyses.R
@@ -1,22 +1,20 @@
 rm(list=ls())
 
-library(readr)
-library(dplyr)
-library(ggplot2)
-library(stats)
-
+library(tidyverse)
+library(ggpubr)
+library(lme4)
+library(lmerTest)
 ##### 1 READ #####
-eva <- read_csv("fullPlotEva_ESy_1980.csv")
-fullPlotData <- read_csv("fullPlotData_ESy_1980.csv")
+eva <- read_csv("../EVA data/fullPlotEva_new.csv")
+fullPlotData <- read_csv("../EVA data/fullPlotData_new.csv")
 
 # Data on which species are neophytes
 native_intra_analysis=F
 if(native_intra_analysis){
-  species_country_status<- read_csv("country_species_ESy_1980.csv", show_col_types = FALSE)
+  species_country_status<- read_csv("../EVA data/country_species_new.csv", show_col_types = FALSE)
 } else{
-  species_country_status<- read_csv("country_species_ESy_1980.csv", show_col_types = FALSE)
+  species_country_status<- read_csv("../EVA data/country_species_new.csv", show_col_types = FALSE)
   species_country_status$Neophyte[species_country_status$Neophyte=="native_intra"] <- "native"
-  # or if we want to do it with intra seperately
 }
 
 # Assign these names to the eva list
@@ -116,7 +114,7 @@ summary(fullPlotData$Date)
 # extra analysis
 answers_by_date <- fullPlotData %>%
   select(Date, PlotObservationID) %>%
-  pad(start_val = min(fullPlotData$Date), end_val = max(fullPlotData$Date), interval = "day") %>%
+  padr::pad(start_val = min(fullPlotData$Date), end_val = max(fullPlotData$Date), interval = "day") %>%
   group_by(Date) %>%
   summarize(Freq=sum(PlotObservationID)) %>%
   mutate(cumulative = cumsum(replace_na(Freq, 0))) |>mutate(rel_cumsum = cumulative/max(cumulative))
@@ -125,20 +123,200 @@ p <- ggplot(answers_by_date, aes(x= Date)) +
   geom_line(aes(y=rel_cumsum), color = "blue")+ scale_x_date(date_breaks = "10 years", date_labels = "%Y")
 p
 
-#ggsave("Cumulative time distribution.jpeg", p, width = 25, height = 15, units = "cm")
-
 
 #### 5. Numbers ####
-##### 5.1 Species #####
-eva_names <- eva |> group_by(name) |> summarise(n =n())
+###### 5.1 Species ######
+eva_names <- eva |> group_by(name, Neophyte) |> summarise(n =n())
 sum(eva_names$n >=30)
+
+analysed <- eva_names[eva_names$n>=30,]
 
 eva_names$div <- eva$div_name[match(eva_names$name, eva$name)]
 eva_names$eive <- eva$eive_name[match(eva_names$name, eva$name)]
 
 sum(!is.na(eva_names$div))/ nrow(eva_names)
-sum(!is.na(eva_names$eive)) / nrow(eva_names)
+sum(!is.na(eva_names$eive)/ nrow(eva_names)
 
 sum(!is.na(eva$eive_name)) / nrow(eva)
 sum(!is.na(eva$div_name)) / nrow(eva)
+
+# read species dominance, which is more correct
+species_dominance <- read.csv("../Results/speciesDominance_1980.csv")
+
+setdiff(species_dominance$names, analysed$name)
+setdiff( analysed$name, species_dominance$names)
+
+#### 6. Traits #####
+###### 6.1 Plot EIVE #######
+colnames(eva)
+
+eva2 <- eva |> group_by(name, Neophyte,eive_name, div_name, EIVEresM, EIVEresN, EIVEresL, EIVEresR, EIVEresT, Disturbance.Severity, Disturbance.Frequency,
+                        Grazing.Pressure, Mowing.Frequency, Soil.Disturbance) |> summarise(n=n())
+
+eva2[duplicated(eva2[,1:2]) | duplicated(eva2[,1:2], fromLast=T) ,]
+table(eva2$Neophyte)
+
+
+names <- c("EIVE-M","EIVE-N","EIVE-L", "EIVE-R","EIVE-T","Disturbance Severity","Disturbance Frequency","Grazing Pressure", "Mowing Frequency","Soil Disturbance")
+phylo <- read_csv("../Extra data/Species names/phylo.csv", show_col_types = F)
+
+library(emmeans)
+library(multcomp)
+
+for(i in 5: 14){
+
+var <- colnames(eva2)[i]
+eva3 <- eva2[, c(1,2,i)]
+colnames(eva3)[3] <- "var"
+eva3 <- eva3[!is.na(eva3$var),]
+
+eva3[, c(4:5)] <- phylo[match(eva3$name, phylo$name), 6:7]
+
+
+# glmm
+model <- lmer(var~ -1+ Neophyte + (1|family/genus), data= eva3 )
+summary(model)
+emm<- emmeans::emmeans(model, pairwise ~ Neophyte)
+
+if(i < 1){
+  y_n <- 10
+  y_let <- 10.2
+} else {
+  y_n <- max(eva3$var)+max(eva3$var)/10
+  y_let <- max(eva3$var)+max(eva3$var)/5
+}
+
+class(emm$emmeans)
+
+cld_results <- cld(emm$emmeans, alpha = 0.05, Letters = letters, adjust = "tukey", reversed=T)
+cld_results$.group <- gsub(" ","",cld_results$.group)
+cld_results
+
+plot <- ggplot(eva3, aes(x=Neophyte, y= var, fill=Neophyte))+
+  geom_violin(alpha=0.5, scale="width") +
+  geom_boxplot(width=0.25, alpha=0.8, fill="white", outlier.shape = NA) +
+  theme_pubr() +
+  stat_summary(fun="mean", geom="point", aes(group=Neophyte), size=3) +
+  scale_colour_manual(values=c( "#004D40","#FFC107", "#1E88E5", "darkgreen")) +
+  scale_fill_manual(values=c( "#004D40","#FFC107", "#1E88E5", "darkgreen")) +
+  guides(color="none") +
+  theme(legend.position="none") +
+  scale_x_discrete(labels=c("Extra-\nEuro pean", "Intra-\nEuropean", "Native", "Native\nalien elsewhere")) +
+  theme(axis.text.x=element_text(size=12), axis.title.y=element_text(size=12), axis.title.x=element_blank(), 
+        axis.text.y=element_text(size=10)) +
+  scale_y_continuous(limits=c(0, y_let+y_let/10))+
+  ylab(names[i-4])+
+  annotate("text", x=1, y=y_n, label=paste("n=", length(eva3$name[eva3$Neophyte=="extra"])), size=4, hjust=0.5)+
+  annotate("text", x=2, y=y_n, label=paste("n=", length(eva3$name[eva3$Neophyte=="intra"])), size=4, hjust=0.5) +
+  annotate("text", x=3, y=y_n, label=paste("n=", length(eva3$name[eva3$Neophyte=="native"])), size=4, hjust=0.5) +
+  annotate("text", x = 1, y = y_let, label = cld_results$.group[cld_results$Neophyte=="extra"], size = 4,vjust = -0.5, hjust = 0.5, alpha=1) +
+  annotate("text", x = 2, y = y_let, label = cld_results$.group[cld_results$Neophyte=="intra"], size = 4,vjust = -0.5, hjust = 0.5, alpha=1) +
+  annotate("text", x = 3, y = y_let, label = cld_results$.group[cld_results$Neophyte=="native"], size = 4,vjust = -0.5, hjust = 0.5, alpha=1) 
+ plot
+
+ assign(gsub(" ","_",var), plot) 
+ 
+}
+
+plot <- ggarrange(EIVEresN, EIVEresR, EIVEresL, EIVEresL, EIVEresM,Disturbance.Severity,  Disturbance.Frequency, 
+                  Grazing.Pressure, Mowing.Frequency, Soil.Disturbance, nrow=5, ncol=2, labels = LETTERS[1:10],
+                  font.label = list(size = 12), align="hv")
+plot
+
+#ggsave("../Images/Species_values.svg", plot= plot, width = 10, height = 10)
+
+
+###### 6.2 Plot Traits #######
+eva_names <- eva |> group_by(name, Neophyte) |>summarise(n=n())
+
+Diaz <- read_csv("../Extra data/Traits/Eva_diaz.csv")
+eva_names <- left_join(eva_names, Diaz[!duplicated(Diaz$name),-c(2:6)], by=c("name"="name"))
+
+mean <- read_csv("../Results/trait_normalized.csv")
+
+eva_names$LA[is.na(eva_names$LA)] <- mean$LA[match(eva_names$name[is.na(eva_names$LA)], mean$SpeciesName)]
+eva_names$LMA[is.na(eva_names$LMA)] <- mean$LMA[match(eva_names$name[is.na(eva_names$LMA)], mean$SpeciesName)]
+eva_names$SSD[is.na(eva_names$SSD)] <- mean$SSD[match(eva_names$name[is.na(eva_names$SSD)], mean$SpeciesName)]
+eva_names$SM[is.na(eva_names$SM)] <- mean$SM[match(eva_names$name[is.na(eva_names$SM)], mean$SpeciesName)]
+eva_names$H[is.na(eva_names$H)] <- mean$H[match(eva_names$name[is.na(eva_names$H)], mean$SpeciesName)]
+eva_names$Nmass[is.na(eva_names$Nmass)] <- mean$Nmass[match(eva_names$name[is.na(eva_names$Nmass)], mean$SpeciesName)]
+
+# standardize
+test <- eva_names
+test[, c(4:9)] <- log(test[,c(4:9)])
+test[, c(4:9)] <- scale(test[, c(4:9)])
+j=4
+# remove outliers (see Diaz et al 2016 for number 4)
+for(j in 4:9){
+  tr <- as.matrix(test[,j])
+  print(sum(is.na(tr)))
+  tr <- ifelse(tr <= -4, NA, tr)
+  tr <- ifelse(tr >= 4, NA, tr)
+  print(sum(is.na(tr)))
+  which(is.na(tr))
+  eva_names[is.na(tr),j] <- NA
+}
+
+phylo <- read.csv("../Extra data/Species names/phylo.csv")
+eva_names[, c(14,13)] <- phylo[match(eva_names$name, phylo$name), 6:7]
+names <- c("Leaf area [mm²]", "LMA [g/m²]","SSD [mg/mm³]","Seed mass [mg]","Leaf N [mg/g]","Height [m]")
+
+i=4
+for(i in 4: 9){
+  
+  var <- colnames(eva_names)[i]
+  eva3 <- eva_names[, c(1,2,i, 14,13)]
+  colnames(eva3)[3:5] <- c("var","genus","family")
+  eva3 <- eva3[!is.na(eva3$var),]
+  
+  
+  # glmm
+  model <- lmer(var~ -1+Neophyte + (1|family/genus), data= eva3 )
+  summary(model)
+  emm<- emmeans::emmeans(model, pairwise ~ Neophyte)
+  
+  if(i < 1){
+    y_n <- 10
+    y_let <- 10.2
+  } else {
+    value <- as.numeric(quantile(eva3$var)[4] + IQR(eva3$var)*1.5)
+    y_n <- value + value/10
+    y_let <- value+ value/5
+  }
+
+  cld_results <- cld(emm$emmeans, alpha = 0.05, Letters = letters, adjust = "tukey", reversed=T)
+  cld_results$.group <- gsub(" ","",cld_results$.group)
+  cld_results
+  
+  plot <- ggplot(eva3, aes(x=Neophyte, y= var, fill=Neophyte))+
+    geom_violin(alpha=0.5, scale="width") +
+    geom_boxplot(width=0.25, alpha=0.8, fill="white", outlier.shape = NA) +
+    theme_pubr() +
+    stat_summary(fun="mean", geom="point", aes(group=Neophyte), size=3) +
+    scale_colour_manual(values=c( "#004D40","#FFC107", "#1E88E5", "darkgreen")) +
+    scale_fill_manual(values=c( "#004D40","#FFC107", "#1E88E5", "darkgreen")) +
+    guides(color="none") +
+    theme(legend.position="none") +
+    scale_x_discrete(labels=c("Extra-\nEuropean", "Intra-\nEuropean", "Native", "Native\nalien elsewhere")) +
+    theme(axis.text.x=element_text(size=12), axis.title.y=element_text(size=12), axis.title.x=element_blank(), 
+          axis.text.y=element_text(size=10)) +
+    scale_y_continuous(limits=c(0, y_let+y_let/10))+
+    ylab(names[i-3])+
+    annotate("text", x=1, y=y_n, label=paste("n=", length(eva3$name[eva3$Neophyte=="extra"])), size=4, hjust=0.5)+
+    annotate("text", x=2, y=y_n, label=paste("n=", length(eva3$name[eva3$Neophyte=="intra"])), size=4, hjust=0.5) +
+    annotate("text", x=3, y=y_n, label=paste("n=", length(eva3$name[eva3$Neophyte=="native"])), size=4, hjust=0.5) +
+    annotate("text", x = 1, y = y_let, label = cld_results$.group[cld_results$Neophyte=="extra"], size = 4,vjust = -0.5, hjust = 0.5, alpha=1) +
+    annotate("text", x = 2, y = y_let, label = cld_results$.group[cld_results$Neophyte=="intra"], size = 4,vjust = -0.5, hjust = 0.5, alpha=1) +
+    annotate("text", x = 3, y = y_let, label = cld_results$.group[cld_results$Neophyte=="native"], size = 4,vjust = -0.5, hjust = 0.5, alpha=1) 
+  plot
+  
+  assign(gsub(" ","_",var), plot) 
+  
+}
+
+plot <- ggarrange(LA,SSD,LMA,SM,Nmass,H, nrow=3, ncol=2, labels = LETTERS[1:10],
+                  font.label = list(size = 12), align="hv")
+plot
+
+#ggsave("../Images/Species_traits.svg", plot= plot, width = 10, height = 6)
 
--- 9.2.2 Gapfilling_evaluate.R
+++ 9.2.2 Gapfilling_evaluate.R
@@ -1,37 +1,33 @@
 
-library(devtools)
-library(readr)
+# This file must be run in R 3.4.4
 library(dplyr)
+library(tibble)
+library(readr)
 library(BHPMF)
-
-set.seed(123)
-
+  
 ######Basic input data
-trait <- read_csv("../TRY/Species_traits_EVA.csv", show_col_types = FALSE)
+trait <- read_csv("../Extra data/Traits/Eva_diaz.csv")
+trait <- trait[!is.na(trait$Genus),]
 #trait <- trait[sample(nrow(trait), round(nrow(trait)/50), replace = F),]
+
+# change wrong family
 trait$Family[trait$Genus=="Antirrhinum"] <- gsub("Scrophulariaceae","Plantaginaceae", trait$Family[trait$Genus=="Antirrhinum"])
 colnames(trait)
-hier <- trait[, c(1, 3,4)]
+
+# create hierarchy
+hier <- trait[, c(1, 17,16)]
 colnames(hier)[1] <- "SpeciesName"
 hier$IDX <- c(1: nrow(hier))
-hier <- hier |> relocate(IDX, .before= SpeciesName)
-phylo <- read_csv("phylo.csv",show_col_types = FALSE)
+hier <- hier[, c(4,1,2,3)]
+phylo <- read_csv("../Extra data/Species names/phylo.csv")
 duplicates <- unique(hier[, 3:4])
 duplicates <- duplicates[duplicated(duplicates$Genus)|duplicated(duplicates$Genus,fromLast=T),]
 
-test <- hier[hier$Genus %in% duplicates$Genus,]
-
-test <- test |> group_by(Genus, Family) |> summarise(n=n())
-test <- test[order(test$n, decreasing=T),]
-test <- test[!duplicated(test$Genus),]
-
-hier$Family[hier$Genus %in% test$Genus] <- test$Family[match(hier$Genus[hier$Genus %in% test$Genus], test$Genus)]
-
-change <- data.frame(Genus = c("Holboellia","Calophyllum","Labisia","Platysace","Ripogonum","Sarcotheca","Sphenostemon","Stauntonia","Tectaria"),
-                     Family= c("Lardizabalaceae","Calophyllaceae","Primulaceae","Apiaceae","Ripogonaceae","Oxalidaceae","Paracryphiaceae","Lardizabalaceae","Polypodiaceae"))
+change <- data.frame(Genus = c("Holboellia","Calophyllum","Labisia","Platysace","Ripogonum","Sarcotheca","Sphenostemon","Stauntonia","Tectaria","Celtis","Digitalis","Hypericum","Melampyrum"),
+                     Family= c("Lardizabalaceae","Calophyllaceae","Primulaceae","Apiaceae","Ripogonaceae","Oxalidaceae","Paracryphiaceae","Lardizabalaceae","Polypodiaceae","Ulmaceae","Plantaginaceae","Hypericaceae","Orobanchaceae"))
 
 hier$Family[hier$Genus %in% change$Genus] <- change$Family[match(hier$Genus[hier$Genus %in% change$Genus], change$Genus)]
-
+colnames(trait)
 hier <- as.matrix(hier)
 trait <- as.matrix(trait[, c(7:12)])
 
@@ -52,7 +48,9 @@ for(i in 1:ncol(trait)){
                                slogx = slogx)
   trait[,i] <- x
 }
-#write.csv(back_trans_pars, paste0("back_trans_pars.csv"))
+
+#saveRDS(back_trans_pars, "../Extra data/Traits/back_trans_pars.rds")
+
 
 ###########10-fold imputation to calculate RMSE
 ####BHPMF output RMSE automatically
@@ -61,23 +59,31 @@ samplenum <- 1000
 burnnum <- 200
 gapnum <- 2
 foldnum <- 10
-GapFilling(as.matrix(trait), hierarchy.info = hier,
+
+GapFilling(as.matrix(trait), hierarchy.info = as.matrix(hier),
            prediction.level = 4, used.num.hierarchy.levels = 3,
            num.samples=samplenum, burn=burnnum, gaps=gapnum,
            num.latent.feats=20, tuning=TRUE, num.folds.tuning=foldnum,rmse.plot.test.data=TRUE,
-           mean.gap.filled.output.path = "C:/Users/u0166342/Documents/Boeren/Impact/eva_neophytes/Intermediate Data/mean_gap_filled.txt",
-           std.gap.filled.output.path= "C:/Users/u0166342/Documents/Boeren/Impact/eva_neophytes/Intermediate Data/std_gap_filled.txt")
+           mean.gap.filled.output.path = "C:/Users/u0166342/Documents/Doctoraat/Impact/Extra data/Traits/Results/mean_gap_filled.txt",
+           std.gap.filled.output.path= "C:/Users/u0166342/Documents/Doctoraat/Impact/Extra data/Traits/Results/std_gap_filled.txt")
 
-rmse_out <- CalculateCvRmse(as.matrix(trait), hier, 
+rmse_out <- CalculateCvRmse(as.matrix(trait), as.matrix(hier), 
                             num.samples=samplenum, burn=burnnum, 
                             gaps=gapnum, num.folds=10, num.latent.feats=15,
-                            tmp.dir="C:/Users/u0166342/Documents/Boeren/Impact/eva_neophytes/Intermediate Data")
+                            tmp.dir="C:/Users/u0166342/Documents/Doctoraat/Impact/Extra data/Traits/Results")
+
+rmse_out[["avg.rmse"]]
+0.4177873
+rmse_out[["std.rmse"]]
+0.02759093
 
 
 # Assign
-mean <- read.delim("C:/Users/u0166342/Documents/Boeren/Impact/eva_neophytes/Intermediate Data/mean_gap_filled.txt")
-#back_trans_pars <- read.csv("back_trans_pars.csv")
+mean <- read.delim("../Extra data/Traits/Results/mean_gap_filled.txt")
+std <- read.csv("../Extra data/Traits/Results/std_gap_filled.txt")
+back_trans_pars <- readRDS( "../Extra data/Traits/back_trans_pars.rds")
 
+# back transform
 for(i in 1:6){
   mean[,i] <- 10^(mean[,i]*back_trans_pars[[i]]$slogx+back_trans_pars[[i]]$mlogx)
 }
@@ -89,9 +95,10 @@ boxplot(mean[,4])
 boxplot(mean[,5])
 boxplot(mean[,6])
 
+hier <- as.data.frame(hier)
 mean <- cbind(hier[, -1], mean)
 
-write_csv(mean, "trait_normalized.csv")
+write_csv(mean, "../Results/trait_normalized.csv")
 
 for(i in 1:6){
   Q <- quantile(mean[,i], probs= c(.25, .75), na.rm=T)
